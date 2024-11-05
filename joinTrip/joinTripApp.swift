e0be43a8c104474e3f5d788ce489b6978861d2ee
michelleswastika <94586964+michelleswastika@users.noreply.github.com>
2024-11-05 08:50:08 +0700

    Initial Commit

---
+++ joinTrip/joinTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  joinTripApp.swift
+//  joinTrip
+//
+//  Created by Michelle Swastika on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct joinTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
